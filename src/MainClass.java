26f238a3bf8c91f319993cc46bf7c78d5ead2706
Alexandr Chekalenko <whibleykevin@gmail.com>
2017-09-01 19:21:32 +0300

    commit with Notepad++

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,9 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by Cagy on 9/1/2017.
  */
 public class MainClass {
-    public static void main(String[] args) {
-         //int result = Add.Plus(7, 6);
-        System.out.println(Add.Plus(7, 6));
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(Add.Plus(Integer.parseInt(reader.readLine()), Integer.parseInt(reader.readLine())));
     }
 }
