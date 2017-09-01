8d33139679c3f2e3b425dd9d41bdc0c7d8d4d43c
Alexandr Chekalenko <whibleykevin@gmail.com>
2017-09-01 18:33:27 +0300

    init

---
+++ src/Add.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Cagy on 9/1/2017.
+ */
+public class Add {
+    public static int Plus(int a, int b){
+        int c;
+        c = a + b;
+
+        return c;
+    }
+}
---
+++ src/MainClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Cagy on 9/1/2017.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+         int result = Add.Plus(7, 6);
+        System.out.println(result);
+    }
+}
